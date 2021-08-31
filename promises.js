f2079d7a46ad06dcce5981649ca5fe34f32c20f1
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-31 20:51:58 +0500

    Implement a resolve promise

--- promises.js
+++ promises.js
@@ -1,2 +1,8 @@
 // Javascript async event handling
-const myPromise = new Promise(() => {});
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('I WON!');
+  }, 1000);
+});
+
+myPromise.then((value) => console.log(value));
