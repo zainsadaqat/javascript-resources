a60eaaf6fe824a264eac4cf07ff2b30dbc726edd
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-31 20:54:33 +0500

    Implement promises reject state

--- promises.js
+++ promises.js
@@ -1,8 +1,14 @@
 // Javascript async event handling
 const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('I WON!');
-  }, 1000);
+  if (false) {
+    setTimeout(() => {
+      resolve('I WON!');
+    }, 1000);
+  } else {
+    reject("I've Failed :(");
+  }
 });
 
-myPromise.then((value) => console.log(value));
+myPromise
+  .then((value) => console.log(value))
+  .catch((rejectValue) => console.log(rejectValue));
