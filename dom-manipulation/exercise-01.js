8e6b20a407c7f3baedd96282d2d7bec53c57d3cf
Zain Sadaqat <zain.sadaqet@gmail.com>
2025-01-09 14:05:21 +0500

    Create an exercise for dom-manipulation

---
+++ dom-manipulation/exercise-01.js
@@ -0,0 +1,18 @@
+/* 
+
+Exercise 1: To-Do List with Dynamic Actions
+
+Scenario: You are building a to-do list app. Users should be able to:
+
+a) Add new tasks.
+b) Mark tasks as completed.
+c) Remove tasks.
+
+Requirements:
+Add a task when the user types in an input field and clicks the "Add Task" button.
+
+Strike through the text of a task when the user clicks on it to mark it as completed.
+
+Add a "Remove" button next to each task. Clicking the "Remove" button deletes the task.
+
+*/
