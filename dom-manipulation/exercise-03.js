8c8cf75607ac24dd2b7cf8643c9c46ca6d74a560
Zain Sadaqat <zain.sadaqet@gmail.com>
2025-01-09 18:40:08 +0500

    Upload exercise-03

---
+++ dom-manipulation/exercise-03.js
@@ -0,0 +1,14 @@
+/*
+Exercise 3: FAQ Accordion
+Scenario: You are building an FAQ section for a website. 
+
+Each question should:
+
+a) Expand to show the answer when clicked.
+b) Collapse to hide the answer when clicked again.
+
+Requirements:
+Create a list of questions with hidden answers.
+Clicking on a question toggles the visibility of its corresponding answer.
+
+*/
