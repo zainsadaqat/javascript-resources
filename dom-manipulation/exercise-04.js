ae7d309ca33c6785345a2df6dfdb7609df8c2602
Zain Sadaqat <zain.sadaqet@gmail.com>
2025-01-09 18:40:18 +0500

    Upload exercise-04

---
+++ dom-manipulation/exercise-04.js
@@ -0,0 +1,14 @@
+/*
+Exercise 4: Image Gallery Lightbox
+Scenario: You are building an image gallery. 
+
+Users should:
+
+a) Click on an image thumbnail to view it in a larger modal.
+b) Close the modal by clicking a close button.
+
+Requirements:
+Display a grid of image thumbnails.
+When a user clicks a thumbnail, display the full image in a modal with a dark background.
+Add a "Close" button to hide the modal.
+*/
